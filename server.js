a39df3a2117a0266a2796e49cba08a99c389212c
Andre <andre.github@gmail.com>
2015-08-15 22:56:25 +0800

    display username

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ var port = process.env.PORT || 18080;
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<h1>Node.js</h1>');
+    res.write('<p>Username:'+process.env.BAE_USERNAME+'</p>');
     res.end('<p>Hello World From Heroku to BAE.</p>');
 }).listen(port);
