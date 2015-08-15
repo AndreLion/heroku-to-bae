47cf076317083d3099136c49ec33a61c091405a0
Andre <andre.github@gmail.com>
2015-08-15 22:50:36 +0800

    fix port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var port = 18080;
+var port = process.env.PORT || 18080;
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<h1>Node.js</h1>');
