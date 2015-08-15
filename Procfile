434c42a11dfe9d713a5e073f19cf3e544d136acb
Andre <andre.github@gmail.com>
2015-08-16 00:37:06 +0800

    4th try

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: node server.js
-worker: git push --mirror "https://$BAE_USERNAME:$BAE_PASSWORD@$BAE_GIT"
+web: node server.js && echo $BAE_USERNAME &&git push --mirror "https://$BAE_USERNAME:$BAE_PASSWORD@$BAE_GIT"
--- server.js
+++ server.js
@@ -4,6 +4,5 @@ http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<h1>Node.js</h1>');
     res.write('<p>username:'+process.env.BAE_USERNAME+'</p>');
-    res.write('<p>password:'+process.env.BAE_PASSWORD+'</p>');
-    res.end('<p>Hello World From Heroku to BAE for 3rd time</p>');
+    res.end('<p>Hello World From Heroku to BAE for 4th time</p>');
 }).listen(port);
