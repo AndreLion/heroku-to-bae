f6e2ead07a9d1b895d98cbc004498c3921c68e56
Andre <andre.github@gmail.com>
2015-08-16 00:07:19 +0800

    2nd try

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var port = process.env.PORT || 18080;
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<h1>Node.js</h1>');
-    res.write('<p>git:'+process.env.BAE_GIT+'</p>');
-    res.end('<p>Hello World From Heroku to BAE for 1st time</p>');
+    res.write('<p>username:'+process.env.BAE_USERNAME+'</p>');
+    res.write('<p>password:'+process.env.BAE_PASSWORD+'</p>');
+    res.end('<p>Hello World From Heroku to BAE for 2nd time</p>');
 }).listen(port);
