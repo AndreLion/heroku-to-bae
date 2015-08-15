235d8717a3fd3ef865beb7765190845c190c7324
Andre <andre.github@gmail.com>
2015-08-15 23:50:58 +0800

    try again

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<h1>Node.js</h1>');
     res.write('<p>git:'+process.env.BAE_GIT+'</p>');
-    res.end('<p>Hello World From Heroku to BAE from LOCAL</p>');
+    res.end('<p>Hello World From Heroku to BAE for 1st time</p>');
 }).listen(port);
